4f1f3f496b2c50c578baaa9eaed4354a090b6056
katchannn <suzunoki24@gmail.com>
2023-06-13 14:15:27 +0900

    簡易的な署名の検証を追加

--- app/main.py
+++ app/main.py
@@ -14,19 +14,52 @@ AWS Cognitoから取得したJWTトークンを検証するサンプル
 
 参考:https://docs.aws.amazon.com/ja_jp/cognito/latest/developerguide/amazon-cognito-user-pools-using-tokens-verifying-a-jwt.html
 """
+
 app = FastAPI()
 
-# APIキーの名前を設定
+# Cognitoの設定
+COGNITO_ISSUER = "https://cognito-idp.{region}.amazonaws.com/{userPoolId}"
+COGNITO_AUDIENCE = "{appId}"
+TOKEN_USE = "access"
+EXPIRATION_TIME = 3600
+
+# APIの設定
 API_KEY_NAME = "Authorization"
 api_key_header_auth = APIKeyHeader(name=API_KEY_NAME, auto_error=True)
 
-# AWS Cognitoの情報
-COGNITO_ISSUER = 'https://cognito-idp.{region}.amazonaws.com/{userPoolId}'
-COGNITO_AUDIENCE = '{appId}'
 
-async def authenticate_token(api_key: str = Depends(api_key_header_auth)) -> Optional[str]:
+def create_token(
+    username: str,
+    secret: str,
+    issuer: str = COGNITO_ISSUER,
+    audience: str = COGNITO_AUDIENCE,
+    token_use: str = TOKEN_USE,
+) -> str:
     """
-    AWS Cognitoから取得したトークンを検証する関数
+    トークンの作成
+    """
+    payload = {
+        "username": username,
+        "iss": issuer,
+        "aud": audience,
+        "exp": int(time.time()) + EXPIRATION_TIME,
+        "token_use": token_use,
+    }
+    return jwt.encode(payload, secret, algorithm="HS256")
+
+
+# デモ用のトークン作成
+username = "your_username"
+secret = "your_secret_key"
+token = create_token(username, secret)
+print(token)
+
+
+async def authenticate_token(
+    api_key: str = Depends(api_key_header_auth), secret_key: str = secret
+) -> Optional[str]:
+    """
+    トークン検証
     """
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
@@ -34,71 +67,25 @@ async def authenticate_token(api_key: str = Depends(api_key_header_auth)) -> Opt
         headers={"WWW-Authenticate": "Bearer"},
     )
     try:
-        # JWTトークンをデコード（署名の検証は省略）
-        token = api_key.split(" ")[1]  # 'Bearer'を取り除く
-        payload = jwt.decode(token, options={"verify_signature": False})
+        token = api_key.split(" ")[1]  # Remove 'Bearer'
+        payload = jwt.decode(token, secret_key, algorithms=["HS256"])
 
-        username: Optional[str] = payload.get("username")
-        
-        if username is None:
+        username = payload.get("username")
+        if (
+            not username
+            or payload["iss"] != COGNITO_ISSUER
+            or payload["aud"] != COGNITO_AUDIENCE
+            or payload["exp"] < time.time()
+            or payload["token_use"] != TOKEN_USE
+        ):
             raise credentials_exception
 
-        # 発行者とオーディエンスを検証
-        if payload['iss'] != COGNITO_ISSUER or payload['aud'] != COGNITO_AUDIENCE:
-            raise credentials_exception
-        
-        # 有効期限とトークン使用用途を検証
-        current_time = time.time()
-        if payload['exp'] < current_time or payload.get('token_use') != 'access':
-            raise credentials_exception
-
-    except InvalidTokenError:
+    except jwt.InvalidTokenError:
         raise credentials_exception
+
     return username
+
 
 @app.get("/", dependencies=[Depends(authenticate_token)])
 def read_root():
     return {"Hello": "World"}
-
-
-# from fastapi import Depends, FastAPI, HTTPException, status, Security
-# from fastapi.security import OAuth2
-# from fastapi.openapi.models import OAuthFlows as OAuthFlowsModel
-# from typing import Optional
-# import jwt
-# from jwt.exceptions import InvalidTokenError
-
-# app = FastAPI()
-
-# class OAuth2PasswordBearerWithJWT(OAuth2):
-#     def __init__(self, scheme_name: str = None, scopes: dict = None, auto_error: bool = True):
-#         if not scheme_name:
-#             scheme_name = self.__class__.__name__
-#         flows = OAuthFlowsModel(password={"tokenUrl": "/token"})
-#         super().__init__(flows=flows, scheme_name=scheme_name, auto_error=auto_error)
-
-# oauth2_scheme = OAuth2PasswordBearerWithJWT()
-
-# # AWS Cognitoの情報
-# COGNITO_ISSUER = 'https://cognito-idp.{region}.amazonaws.com/{userPoolId}'
-# COGNITO_AUDIENCE = '{appId}'
-
-# async def authenticate_token(token: str = Depends(oauth2_scheme)) -> Optional[str]:
-#     credentials_exception = HTTPException(
-#         status_code=status.HTTP_401_UNAUTHORIZED,
-#         detail="Could not validate credentials",
-#         headers={"WWW-Authenticate": "Bearer"},
-#     )
-#     try:
-#         payload = jwt.decode(token, options={"verify_signature": False})
-#         username: Optional[str] = payload.get("username")
-
-#         if username is None or payload['iss'] != COGNITO_ISSUER or payload['aud'] != COGNITO_AUDIENCE:
-#             raise credentials_exception
-#     except InvalidTokenError:
-#         raise credentials_exception
-#     return username
-
-# @app.get("/", dependencies=[Depends(authenticate_token)])
-# def read_root():
-#     return {"Hello": "World"}
